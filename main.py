9c7a4c4348b2bb272d9edc00245442b18474c76b
Afolabi Ikeoluwa <105176572+Fikky1@users.noreply.github.com>
2022-05-28 18:32:13 +0100

    Add files via upload
    classes and object in python

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 
 
 
-class Student:(self, name, age, track, score, float)
+class Student:(self, name, age, track, score,)
     # [assignment] Skeleton class. Add your code here
 def __init__(self):
-        self.name:str=name
-        self.age:int=age
-        self.track:list=track
-        self.score:float=score
+        self.name = str(name)
+        self.age = int(age)
+        self.track = list(track)
+        self.score = float(score)
 
-        self=input_attribute
+def change_name(self,new_name):
+        self.change_name = new_name
+        print("student name is", name)
+def change_age(self,new_age):
+        self.change_name = new_age
+        print("the age is",new_age)
 
-def change_name(self,name):
-        self.name= name
-        print("student name", self.name)
-def change_age(self,age):
-        self.age= age
-        print("the age is", age)
+def add_track(self,new_track):
+        self.change_track = new_track
+        print(" the track is", new_tracks)
 
-def add_track(self,track):
-        self.track=self.track.append
-        print(" the track is", tracks)
-
-def get_score(self,score):
-        self_score=score
-        print("the students score is", score)
+def get_score(self,new_score):
+        self.get_score = new_score
+        print("the students score is",new_score)
     
